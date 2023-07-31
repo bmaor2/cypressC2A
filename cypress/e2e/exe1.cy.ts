56f79aa105a1e82ac3f8926a5c9e2eb8c8faf992
Bar Maor <bmaor2@gmail.com>
2023-07-31 16:30:14 +0300

    finished Exercise 1

--- cypress/e2e/exe1.cy.ts
+++ cypress/e2e/exe1.cy.ts
@@ -3,12 +3,13 @@
 //PATHS
 const FILE_1_PATH = "files/file1.pdf";
 const FILE_2_PATH = "files/file2.pdf";
+const FILE_3_PATH = "files/file3-notEquals.pdf";
 
-describe("Exercise 1:", () => {
-  it("Should compare and assert between 2 PDF files",() => {
-    cy.readFile(FILE_1_PATH).then((file1) => {
-      cy.readFile(FILE_2_PATH).then((file2) => {
-        expect(file1).to.equal(file2);
+describe("Exercise 1:", async () => {
+  it("Should compare between 2 PDF files and assert true if they are equal", () => {
+    cy.readFile(FILE_1_PATH, "base64").then(async (file1) => {
+      cy.readFile(FILE_3_PATH, "base64").then((file2) => {
+        cy.log("Is the files equals? : " + String(file1 === file2));
       });
     });
   });
